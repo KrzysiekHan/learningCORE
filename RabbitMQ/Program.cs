fc9276ed24065e7ad37d244e3d0287cfc6abb737
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-02-15 14:12:01 +0100

    rabbit connection test

--- RabbitMQ/Program.cs
+++ RabbitMQ/Program.cs
@@ -9,7 +9,7 @@ namespace RabbitMQ
     class Program
     {
         // CloudAMQP URL in format amqp://user:pass@hostName:port/vhost
-        static string url = "amqp://pxnpxzlt:F2VzPK13wRKw20APTOy3Dk359yTJQ8QD@hawk.rmq.cloudamqp.com/pxnpxzlt";
+        static string url = "amqps://pxnpxzlt:F2VzPK13wRKw20APTOy3Dk359yTJQ8QD@hawk.rmq.cloudamqp.com/pxnpxzlt";
         static readonly ConnectionFactory connFactory = new ConnectionFactory() {
             Uri = new Uri(url)
         };
@@ -24,36 +24,40 @@ namespace RabbitMQ
         public static void Publish()
         {
             using (var conn = connFactory.CreateConnection())
-            using (var channel = conn.CreateModel())
             {
-                var message = DateTime.Now.ToLongDateString();
-                var data = Encoding.UTF8.GetBytes(message);
-                var queueName = "queue1";
-                bool durable = true;
-                bool exclusive = false;
-                bool autoDelete = false;
-                channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
-                var exchangeName = "";
-                var routingKey = "queue1";
-                channel.BasicPublish(exchangeName, routingKey, null, data);
+                using (var channel = conn.CreateModel())
+                {
+                    var message = DateTime.Now.ToLongDateString();
+                    var data = Encoding.UTF8.GetBytes(message);
+                    var queueName = "queue1";
+                    bool durable = true;
+                    bool exclusive = false;
+                    bool autoDelete = false;
+                    channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
+                    var exchangeName = "";
+                    var routingKey = "queue1";
+                    channel.BasicPublish(exchangeName, routingKey, null, data);
+                }
             }
         }
 
         public static string GetMessage()
         {
             using (var conn = connFactory.CreateConnection())
-            using (var channel = conn.CreateModel())
             {
-                channel.QueueDeclare("queue1", false, false, false, null);
-                var queueName = "queue1";
-                var data = channel.BasicGet(queueName, false);
-                if (data == null)
+                using (var channel = conn.CreateModel())
                 {
-                    return "no message found";
+                    channel.QueueDeclare("queue1", false, false, false, null);
+                    var queueName = "queue1";
+                    var data = channel.BasicGet(queueName, false);
+                    if (data == null)
+                    {
+                        return "no message found";
+                    }
+                    var message = Encoding.UTF8.GetString(data.Body);
+                    channel.BasicAck(data.DeliveryTag, false);
+                    return message;
                 }
-                var message = Encoding.UTF8.GetString(data.Body);
-                channel.BasicAck(data.DeliveryTag, false);
-                return message;
             }
         }
     }
