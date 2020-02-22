88696e177f8b4df7c6b713c00ac9e3dc51f0a109
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-02-22 17:33:41 +0100

    create WEBAPI

--- RawRabbit_FirstTests/Program.cs
+++ RawRabbit_FirstTests/Program.cs
@@ -13,47 +13,47 @@ namespace RawRabbit_FirstTests
 
 		public static void Main(string[] args)
 		{
-			RunAsync().GetAwaiter().GetResult();
+			//RunAsync().GetAwaiter().GetResult();
 		}
 
-		public static async Task RunAsync()
-		{
-			Log.Logger = new LoggerConfiguration()
-				.WriteTo.LiterateConsole()
-				.CreateLogger();
+		//public static async Task RunAsync()
+		//{
+		//	Log.Logger = new LoggerConfiguration()
+		//		.WriteTo.LiterateConsole()
+		//		.CreateLogger();
 
-			_client = RawRabbitFactory.CreateSingleton(new RawRabbitOptions
-			{
-				ClientConfiguration = new ConfigurationBuilder()
-					.SetBasePath(Directory.GetCurrentDirectory())
-					.AddJsonFile("rawrabbit.json")
-					.Build()
-					.Get<RawRabbitConfiguration>(),
-				Plugins = p => p
-					.UseGlobalExecutionId()
-					.UseMessageContext<MessageContext>()
-			});
+		//	_client = RawRabbitFactory.CreateSingleton(new RawRabbitOptions
+		//	{
+		//		ClientConfiguration = new ConfigurationBuilder()
+		//			.SetBasePath(Directory.GetCurrentDirectory())
+		//			.AddJsonFile("rawrabbit.json")
+		//			.Build()
+		//			.Get<RawRabbitConfiguration>(),
+		//		Plugins = p => p
+		//			.UseGlobalExecutionId()
+		//			.UseMessageContext<MessageContext>()
+		//	});
 
-			await _client.SubscribeAsync<ValuesRequested, MessageContext>((requested, ctx) => ServerValuesAsync(requested, ctx));
-			await _client.RespondAsync<ValueRequest, ValueResponse>(request => SendValuesThoughRpcAsync(request));
-		}
+		//	await _client.SubscribeAsync<ValuesRequested, MessageContext>((requested, ctx) => ServerValuesAsync(requested, ctx));
+		//	await _client.RespondAsync<ValueRequest, ValueResponse>(request => SendValuesThoughRpcAsync(request));
+		//}
 
-		private static Task<ValueResponse> SendValuesThoughRpcAsync(ValueRequest request)
-		{
-			return Task.FromResult(new ValueResponse
-			{
-				Value = $"value{request.Value}"
-			});
-		}
+		//private static Task<ValueResponse> SendValuesThoughRpcAsync(ValueRequest request)
+		//{
+		//	return Task.FromResult(new ValueResponse
+		//	{
+		//		Value = $"value{request.Value}"
+		//	});
+		//}
 
-		private static Task ServerValuesAsync(ValuesRequested message, MessageContext ctx)
-		{
-			var values = new List<string>();
-			for (var i = 0; i < message.NumberOfValues; i++)
-			{
-				values.Add($"value{i}");
-			}
-			return _client.PublishAsync(new ValuesCalculated { Values = values });
-		}
+		//private static Task ServerValuesAsync(ValuesRequested message, MessageContext ctx)
+		//{
+		//	var values = new List<string>();
+		//	for (var i = 0; i < message.NumberOfValues; i++)
+		//	{
+		//		values.Add($"value{i}");
+		//	}
+		//	return _client.PublishAsync(new ValuesCalculated { Values = values });
+		//}
 	}
 }
